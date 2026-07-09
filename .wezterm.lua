6d74505c519a5ef7e6481808b7e0190f4216f499
anthony <6994465+achao-io@users.noreply.github.com>
2026-07-09 02:12:20 -0400

    Modify WezTerm settings for opacity and scrollback
    Updated WezTerm configuration for appearance and performance.

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,15 +7,20 @@ config.color_scheme = "Catppuccin Frappe"
 -- config.color_scheme = "Catppuccin Macchiato"
 -- config.color_scheme = "Catppuccin Mocha"
 
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 20
+
 config.font_size = 14
 config.max_fps = 120
 config.font = wezterm.font("DroidSansMono Nerd Font")
 
-config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_frame = {
 	font = wezterm.font("DroidSansMono Nerd Font", { weight = "Bold" }),
 }
+
+-- More scrollback (default is 3500 lines)
+config.scrollback_lines = 10000
 
 return config
