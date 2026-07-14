c86c6c6e62b8cbf446386a2734c2ee9e4c8ebd00
anthony <6994465+achao-io@users.noreply.github.com>
2026-07-14 15:48:57 -0700

    Update wezterm configuration for color scheme and settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,15 +2,16 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- https://www.reddit.com/r/neovim/comments/v0f4ko/oc_catppuccin_the_lovely_pastel_theme_now_comes/
+-- <leader>uC
 -- config.color_scheme = "Catppuccin Latte"
-config.color_scheme = "Catppuccin Frappe"
+-- config.color_scheme = "Catppuccin Frappe"
 -- config.color_scheme = "Catppuccin Macchiato"
--- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Catppuccin Mocha"
 
 config.window_background_opacity = 0.95
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 95
 
-config.font_size = 14
+config.font_size = 16
 config.max_fps = 120
 config.font = wezterm.font("DroidSansMono Nerd Font")
 
@@ -21,6 +22,6 @@ config.window_frame = {
 }
 
 -- More scrollback (default is 3500 lines)
-config.scrollback_lines = 10000
+config.scrollback_lines = 9999999
 
 return config
